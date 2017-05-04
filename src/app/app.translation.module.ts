eebc4aa77697d4ec25cc26718a1e4cd90db535e8
newmann <newmannhu@qq.com>
2017-05-04 23:05:28 +0800

    created 20170504

--- src/app/app.translation.module.ts
+++ src/app/app.translation.module.ts
@@ -6,7 +6,7 @@ import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { TranslateService } from '@ngx-translate/core';
 
 export function createTranslateLoader(http: Http) {
-    return new TranslateHttpLoader(http, './assets/i18n/US/', '.json');
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 const translationOptions = {
@@ -24,8 +24,12 @@ const translationOptions = {
 })
 export class AppTranslationModule {
   constructor(private translate: TranslateService) {
-    translate.addLangs(["en"]);
-    translate.setDefaultLang('en');
-    translate.use('en');
+    translate.addLangs(["zh-CN","en"]);
+    // translate.setDefaultLang('en');
+    translate.setDefaultLang('zh-CN');
+    //获取当前浏览器环境的语言比如en、 zh
+    let broswerLang = translate.getBrowserLang();
+    translate.use(broswerLang.match(/en|zh-CN/) ? broswerLang : 'zh-CN');
+    // translate.use('en');
   }
 }
